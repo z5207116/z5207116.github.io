482bd35449168f18765f8243a266e7497909928c
z5207116 <z5207116@ad.unsw.edu.au>
2022-11-07 22:08:20 +1100

    first init

---
+++ index.js
@@ -0,0 +1,61 @@
+//template_r09kzmv
+//service_wgej85t
+//GkZly26lEWS7Cwlxy
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_wgej85t",
+      "template_r09kzmv",
+      event.target,
+      "GkZly26lEWS7Cwlxy"
+    )
+    .then(() => {
+      throw new Error("error");
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "the email service is temporarily unavailable Please try again later"
+      );
+    });
+}
+
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += "modal--open";
+}
+
+let contrastToggle = false;
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += "dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
+
+const scaleFactor = 1 / 20;
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; i++) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px,${y * boolInt}px)`;
+  }
+}
